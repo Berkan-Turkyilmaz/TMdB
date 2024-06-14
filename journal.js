e793542c540b1b77f887afe8f8d8f00b2e947b5e
Berkan-Turkyilmaz <brkntrkylmz@gmail.com>
2024-06-14 14:14:47 +0200

    Update journal.js

--- journal.js
+++ journal.js
@@ -14,18 +14,18 @@ function showFavouriteMovies() {
     favMoviesArray.forEach(film => {
         const { overview, original_title, poster_path, vote_average } = film;
         const card = document.createElement('div');
-        card.classList.add('bg-gray-400', 'rounded-lg', 'overflow-hidden', 'shadow-lg');
+        card.classList.add('bg-gray-400', 'rounded-lg','text-black', 'overflow-hidden', 'shadow-lg');
 
         card.innerHTML = `
             <div class="h-96">
                 <img class="w-full h-full object-cover" src="${IMAGE_URL + poster_path}" alt="${original_title}">
             </div>
             <div class="flex flex-col justify-between p-4">
-                <div>
-                    <h2 class="text-xl font-bold">${original_title}</h2>
-                    <p class="text-sm">${vote_average}/10</p>
+                <div class="flex justify-between mb-6 font-bold text-2xl">
+                    <h2>${original_title}</h2>
+                    <p>${vote_average}/10</p>
                 </div>
-                <p class="text-sm overflow-hidden overflow-ellipsis">${overview}</p>
+                <p class="font-semibold text-sm overflow-hidden overflow-ellipsis">${overview}</p>
             </div>
         `;
         movieContainer.appendChild(card);
